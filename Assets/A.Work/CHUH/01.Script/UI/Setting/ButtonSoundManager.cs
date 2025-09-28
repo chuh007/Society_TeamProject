f8b57635b88f36b86a8eaeeec773c65a8fd10aba
chuh007 <chhuh007@gmail.com>
2025-09-28 22:57:09 +0900

    feat : ButtonSound

---
+++ Assets/A.Work/CHUH/01.Script/UI/Setting/ButtonSoundManager.cs
@@ -0,0 +1,42 @@
+﻿using Ami.BroAudio;
+using UnityEditor;
+using UnityEditor.Events;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace A.Work.CHUH._01.Script.UI.Setting
+{
+    public class ButtonSoundManager : MonoBehaviour
+    {
+        [SerializeField] private SoundID buttonSound;
+        
+        public void PlayButtonSound()
+        {
+            BroAudio.Play(buttonSound);
+        }
+        
+#if UNITY_EDITOR
+        [ContextMenu("Auto Button Sound Setting")]
+        private void AutoButtonSound()
+        {
+            var scene = SceneManager.GetActiveScene();
+            var roots = scene.GetRootGameObjects();
+            
+            foreach (var root in roots)
+            {
+                var buttons = root.GetComponentsInChildren<Button>(true);
+                foreach (var btn in buttons)
+                {
+                    UnityEventTools.AddPersistentListener(btn.onClick, PlayButtonSound);
+                    EditorUtility.SetDirty(btn);
+                }
+            }
+            
+            EditorSceneManager.MarkSceneDirty(scene);
+        }
+#endif
+        
+    }
+}
