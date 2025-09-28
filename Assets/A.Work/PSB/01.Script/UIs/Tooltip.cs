6ff8e7a69c80bbb38a749eddce3709cc35d4375c
PSB08 <Parkseongbin08@gmail.com>
2025-09-28 21:37:23 +0900

    feat : Icon tooltip

---
+++ Assets/A.Work/PSB/01.Script/UIs/Tooltip.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Code.Scripts.UI
+{
+    public class Tooltip : MonoBehaviour
+    {
+        [field: SerializeField] public RectTransform RectTransform { get; private set; }
+        [field: SerializeField, Range(0, 1)] public float HoverDelay { get; private set; } = 0.5f;
+        [SerializeField] private TextMeshProUGUI tooltipText;
+        [SerializeField] private string tooltipString;
+
+        private void Awake()
+        {
+            RectTransform = GetComponent<RectTransform>();
+            SetText(tooltipString);
+        }
+
+        public void SetText(string text)
+        {
+            tooltipText.SetText(text);
+            Vector2 preferredSize = tooltipText.GetPreferredValues();
+            RectTransform.sizeDelta = new Vector2(preferredSize.x + 50, preferredSize.y);
+        }
+        
+        public void Show() => gameObject.SetActive(true);
+        public void Hide() => gameObject.SetActive(false);
+        
+    }
+}
---
+++ Assets/A.Work/PSB/01.Script/UIs/TooltipButtonUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Code.Scripts.UI
+{
+    public class TooltipButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Tooltip tooltip;
+        
+        private RectTransform _rectTrm;
+        public bool IsActive { get; private set; }
+
+        private void Awake()
+        {
+            _rectTrm = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            IsActive = true;
+        }
+
+        private void OnDisable()
+        {
+            IsActive = false;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (tooltip != null && IsActive)
+            {
+                Invoke(nameof(ShowTooltip), tooltip.HoverDelay);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            CancelInvoke();
+            tooltip?.Hide();
+        }
+
+        private void ShowTooltip()
+        {
+            tooltip.Show();
+            tooltip.RectTransform.position = new Vector2
+            (
+                _rectTrm.position.x + _rectTrm.rect.width * 0.5f,
+                _rectTrm.position.y + _rectTrm.rect.height * 0.5f
+            );
+        }
+        
+        
+    }
+}
