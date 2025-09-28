9938cf1bf06098959701ff21456379fcc4e2c605
PSB08 <Parkseongbin08@gmail.com>
2025-09-28 23:09:57 +0900

    feat : Exit panel

--- Assets/A.Work/PSB/01.Script/UIs/ExitPanel.cs
+++ Assets/A.Work/PSB/01.Script/UIs/ExitPanel.cs
@@ -9,26 +9,31 @@ namespace Code.Scripts.UI
     public class ExitPanel : MonoBehaviour
     {
         [SerializeField] private GameObject exitPanel;
+        [SerializeField] private Button closePanelBtn;
         [SerializeField] private Button exitButton;
         [SerializeField] private Button lobbyButton;
 
         private void Awake()
         {
             exitPanel.SetActive(false);
+            closePanelBtn.onClick.AddListener(CloseExitPanel);
             exitButton.onClick.AddListener(ExitScene);
             lobbyButton.onClick.AddListener(LobbyScene);
         }
 
         private void OnDestroy()
         {
+            closePanelBtn.onClick.RemoveListener(CloseExitPanel);
             exitButton.onClick.RemoveListener(ExitScene);
             lobbyButton.onClick.RemoveListener(LobbyScene);
         }
 
         public void OpenExitPanel()
         {
-            exitPanel.transform.localScale = Vector3.one;
+            exitPanel.transform.localScale = Vector3.zero;
             exitPanel.gameObject.SetActive(true);
+            
+            exitPanel.transform.DOScale(Vector3.one, 0.5f);
         }
         
         private void CloseExitPanel()
